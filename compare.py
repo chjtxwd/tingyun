05b427e0f2fef4950a7909122ad79908371f742e
haijin cheng <chjtxwd@vip.qq.com>
2021-10-31 12:43:46 +0000

    chaneg few codes

--- compare.py
+++ compare.py
@@ -4,36 +4,16 @@ import numpy as np
 
 urllist=[]
 timelist=[]
-data = pd.read_csv(r'/root/tingyun/exportElem.csv',index_col='时间')
+data = pd.read_csv(r'/root/tingyun/exportElem.csv')
 
-# add all url in list
-for each in data.元素URL:
-  if each not in urllist:
-    urllist.append(each)
-
-# add all time in list:
-
-for each in data.时间:
-  if each not in timelist:
-    timelist.append(each)
-
-#def sort():
+url_list=data['元素URL'].unique()
     
+tests=data['时间'].unique()
+for test in tests:
+  test_data=data[data['时间']==test]
+  for url in url list
 
 
 
 
 
-
-
-
-for each in timelist:
-    test=data.query("时间==@each")
-    for onetest in test:
-        result = pd.DataFrame()
-        # get down 3 kb
-        curl3 = test[test['元素URL'].str.contains('com/F3KB')]
-        curl3 = curl3.sort_values(by='总下载时间')
-       # result=result.
-
-result=pd.concat(result)
